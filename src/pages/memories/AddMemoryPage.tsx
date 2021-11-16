07318c6a8bc3a7d0bdfe8884f007017caa6df402
Nick Lemmon <nick.lemmon@gmail.com>
2021-11-16 07:16:02 -0500

    Removes unused import

--- src/pages/memories/AddMemoryPage.tsx
+++ src/pages/memories/AddMemoryPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { ArrowBackIcon } from '@chakra-ui/icons'
 import { Form, MultiSelect, Page } from 'src/components'
 import {
