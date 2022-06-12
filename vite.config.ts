f14d6e30ca82240b55ede050403ad57be0a065b8
Nick Lemmon <nick.lemmon@gmail.com>
2022-06-12 19:50:50 -0400

    Exclude parse from dep optimization

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  optimizeDeps: {
+    exclude: ['parse'],
+  },
   plugins: [react(), tsconfigPaths()],
 })
