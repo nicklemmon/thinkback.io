eefe8887cb4f69615667e6c16372f1160feae4cd
Nick Lemmon <nick.lemmon@gmail.com>
2022-06-12 19:54:04 -0400

    Exclude xstate from dep opt

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   optimizeDeps: {
-    exclude: ['parse'],
+    exclude: ['xstate'],
   },
   plugins: [react(), tsconfigPaths()],
 })
